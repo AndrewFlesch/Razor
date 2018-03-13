4a948921ee36d3959edbdc3685d09e3816a64e99
AndrewFlesch <aflesch@outlook.com>
2018-03-13 15:49:17 -0500

    Adding framework to empty project

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Describtion = "A boat for one person",
+                Cateogry = "Watersports",
+                Price = 275M
+
+            };
+
+            return View(myProduct);
+        }
+    }
+}
